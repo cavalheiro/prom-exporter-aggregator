78e309102e6a81a05a4376f943845949d183b3c8
Joao Cavalheiro <jcavalheiro@suse.de>
2019-08-21 17:54:20 +0100

    Fetch and process metrics
    - Fetch metrics from configured URLs
    - Add prefix to metric names

--- prom-exporter-aggregator.go
+++ prom-exporter-aggregator.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"fmt"
 	"flag"
+	"regexp"
+	"strings"
 	"net/http"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
@@ -32,25 +34,36 @@ func main() {
 	var configFile = flag.String("config", DEFAULT_CONFIG_FILE, "Path to config file")
 	flag.Parse()
 	// Load configuration file
-	config := []string{}
+	config := map[string]string{}
 	dat, err := ioutil.ReadFile(*configFile)
 	assertNoError(err, "Unable to open configuration file. Use --config=file.yml")
 	err = yaml.Unmarshal([]byte(dat), &config)
 	assertNoError(err, "Invalid configuration file syntax")
 
-	metrics := map[string]string{}
+	// metrics := map[string]string{}
 
-	for _, url := range config {
+	r := regexp.MustCompile(`(?:(#\s(?:TYPE|HELP))\s)?(\w+)\s(.*)`)
+
+	for url, alias := range config {
 		response, err := http.Get(url)
 		logError(err)
 		if err == nil {
 			defer response.Body.Close()
 			contents, err := ioutil.ReadAll(response.Body)
 			logError(err)
-			if err == nil {
-				// Process contents
-
-				fmt.Printf("%s\n", string(contents))
+			if err == nil && (response.StatusCode == 200) {
+				for _, line := range strings.Split(string(contents),"\n") {
+					// fmt.Printf("%s\n", line)
+					result:= r.FindStringSubmatch(line)
+					if (result != nil) {
+						metric_name := fmt.Sprintf("%s_%s", alias, result[2])
+						reg := []string {result[1], metric_name, result[3]}
+						fmt.Printf("%s\n", strings.TrimSpace(strings.Join(reg[:], " ")))
+					}
+					// for k, v := range result {
+					// 	fmt.Printf("%d. %s\n", k, v)
+					// }
+				}
 			}
 		}
 	}
