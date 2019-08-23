b33cf3be06fa4759de0ab666c5754bef4b692502
Joao Cavalheiro <jcavalheiro@suse.de>
2019-08-23 17:44:24 +0100

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# prom-exporter-aggregator-
-A way to expose metrics from multiple Prometheus exporters on a single endpoint
+# Prometheus Exporter Aggregator
+
+A way to expose metrics from multiple Prometheus exporters running on the same host, on a single endpoint.
+
+## Why?
+
+Sometimes you need to run multiple Prometheus exporters on a single machine, resulting in multiple ports having to be exposed, and multiple Prometheus queries hitting that host. 
+
+While the "official" approach to such scenarios is to run an additional Prometheus server on the host and configure it to scrape the local targets, this might be too complex or undesirable for scenarios where a simple metric aggregation could work.
+
+## How it works
+
+It works by querying the endpoints defined on the configuration file, aggregating the metrics, and exposing the consolidated output on a single, configurable port. All queries are run in parallel, and it has a very low footprint. 
+
+Since different exporters running on same host can, in some cases, have common metric names, it is possible to define aliases for each endpoint that will be used to prefix the metric names.
+
+## Configuration 
+
+The configuration file is defined as a YAML map 
+
+```yaml
+http://url:port1 : alias1
+http://url:port2 : alias2
+```
+
+The aliases are optional. If specified, the metrics from each endpoint will be prefixed with the alias. 
+
+## How to run
+
+`$ go run prom-exporter-aggregator.go`
+
+#### Command line options
+
+```
+-config
+    Path to config file (default "prom-exporter-aggregator.yml")
+-port
+    Port to listen on (default "9191")
+``` 
--- prom-exporter-aggregator.go
+++ prom-exporter-aggregator.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"flag"
-	"sync"
-	"time"
 	"bytes"
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"regexp"
 	"strings"
-	"net/http"
-	"io/ioutil"
-	"gopkg.in/yaml.v2"
+	"sync"
+	"time"
 )
 
 const DEFAULT_CONFIG_FILE = "prom-exporter-aggregator.yml"
@@ -27,7 +27,7 @@ func assertNoError(e error, msg string) {
 func main() {
 
 	// Parse command line arguments
-	var port = flag.String("port", "9191", "Port to listen at")
+	var port = flag.String("port", "9191", "Port to listen on")
 	var configFile = flag.String("config", DEFAULT_CONFIG_FILE, "Path to config file")
 	flag.Parse()
 	// Load configuration file
@@ -40,7 +40,7 @@ func main() {
 	var wg sync.WaitGroup
 	regex := regexp.MustCompile(`(?:(#\s(?:TYPE|HELP))\s)?(\w+)\s(.*)`)
 
-	http.HandleFunc("/metrics", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		var metrics bytes.Buffer
 		wg.Add(len(config))
 		for url, alias := range config {
@@ -54,17 +54,17 @@ func main() {
 					defer response.Body.Close()
 					contents, err := ioutil.ReadAll(response.Body)
 					if err != nil || response.StatusCode != 200 {
-						fmt.Printf("ERROR: Invalid response from %s\n",  url)
+						fmt.Printf("ERROR: Invalid response from %s\n", url)
 					} else {
-						for _, line := range strings.Split(string(contents),"\n") {
-							all_tokens:= regex.FindStringSubmatch(line)
-							if (all_tokens != nil) {
+						for _, line := range strings.Split(string(contents), "\n") {
+							all_tokens := regex.FindStringSubmatch(line)
+							if all_tokens != nil {
 								metric_name := fmt.Sprintf("%s_%s", alias, all_tokens[2])
-								wanted_tokens := []string {all_tokens[1], metric_name, all_tokens[3]}							
+								wanted_tokens := []string{all_tokens[1], metric_name, all_tokens[3]}
 								metrics.WriteString(strings.TrimSpace(strings.Join(wanted_tokens, " ")) + "\n")
 							}
 						}
-						fmt.Printf("INFO: Query to endpoint %s took %s\n",  url, time.Since(reqStartTime))
+						fmt.Printf("INFO: Query to endpoint %s took %s\n", url, time.Since(reqStartTime))
 					}
 				}
 			}(url, alias)
@@ -72,5 +72,5 @@ func main() {
 		wg.Wait()
 		fmt.Fprintf(w, metrics.String())
 	})
-	http.ListenAndServe(":" + *port, nil)
+	http.ListenAndServe(":"+*port, nil)
 }
